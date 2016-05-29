9b5761cece88201bfd96be049e25bc32257bcd74
Florent Vilmart <florentvilmart@me.com>
2016-05-29 09:23:33 -0400

    Fixes #1840

--- src/RestWrite.js
+++ src/RestWrite.js
@@ -740,9 +740,7 @@ RestWrite.prototype.runDatabaseOperation = function() {
     .then(response => {
       response.updatedAt = this.updatedAt;
       if (this.storage.changedByTrigger) {
-        Object.keys(this.data).forEach(fieldName => {
-          response[fieldName] = response[fieldName] || this.data[fieldName];
-        });
+        updateResponseWithData(response, this.data);
       }
       this.response = { response };
     });
@@ -766,9 +764,7 @@ RestWrite.prototype.runDatabaseOperation = function() {
       response.objectId = this.data.objectId;
       response.createdAt = this.data.createdAt;
       if (this.storage.changedByTrigger) {
-        Object.keys(this.data).forEach(fieldName => {
-          response[fieldName] = response[fieldName] || this.data[fieldName];
-        });
+        updateResponseWithData(response, this.data);
       }
       this.response = {
         status: 201,
@@ -856,6 +852,19 @@ RestWrite.prototype.cleanUserAuthData = function() {
     }
   }
 };
+
+function updateResponseWithData(response, data) {
+  Object.keys(data).forEach(fieldName => {
+    let dataValue = data[fieldName];
+    let responseValue = response[fieldName];
+    if (dataValue && dataValue.__op === 'Delete') {
+      delete response[fieldName];
+    } else {
+      response[fieldName] = responseValue || dataValue;
+    }
+  });
+  return response;
+}
 
 export default RestWrite;
 module.exports = RestWrite;
